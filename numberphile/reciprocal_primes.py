ad7cc1d68efd4abacdf692d344f25f5ecaada7a2
emuro <emmuro@gmail.com>
2022-04-24 23:43:11 +0200

    "reciprocal primes"

---
+++ numberphile/reciprocal_primes.py
@@ -0,0 +1,46 @@
+import sys
+from decimal import *
+import re
+
+# FUNCTIONS
+###########
+def prime_numbers_up_to_max(p_max):
+    """" calcula todos los primos <= p_max
+        utilizando el método de la criba de Erastótenes.
+        Llega bien hasta  10e7...~6sec en spica.
+    """
+    p = [1] * (p_max+1)  # max prime number al que llego bien solo por mem. es 10e8...~3sec.
+    p[0] = 0
+    p[1] = 0
+
+    for i in range(2, p_max+1):
+        if p[i] == 1:
+            if 2*i <= p_max+1:
+                for j in list(range(2*i, p_max+1, i)):
+                    if p[j] == 0:
+                        continue
+                    else:
+                        p[j] = 0
+                        continue
+        elif p[i] == 0:
+            continue
+
+    primes = []
+    for i in range(0, p_max+1):
+        if p[i] == 1:
+            primes.append(i)
+    print("There are", len(primes), "prime numbers <=", p_max)
+    return primes
+
+
+primes = prime_numbers_up_to_max(70000)
+getcontext().prec = 500
+for i, p in enumerate(primes[3:]):
+    if p>150:
+        sys.exit()
+    r = str(Decimal(1)/Decimal(p))
+    m = re.search(r"^0\.(\d+?)\1+", r)
+    pattern = m.group(1)
+
+    print(i, p, len(pattern), pattern, r)
+
