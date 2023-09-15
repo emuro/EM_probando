fadd2cf883cfe7b8f04a63db520ec7b344070695
emuro <emmuro@gmail.com>
2023-09-15 14:33:20 +0200

    gitPasswd

--- EM_pygame_test.py
+++ EM_pygame_test.py
@@ -20,7 +20,7 @@ while running:
     screen.fill((255, 255, 255))
 
     # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 95)
 
     # Flip the display
     pygame.display.flip()
