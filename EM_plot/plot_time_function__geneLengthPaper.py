4e335d55b9ae5bca9b8d9303f710f69d21c9a16d
emuro <emmuro@gmail.com>
2023-10-19 14:59:47 +0200

    plot_time_function2

--- EM_plot/plot_time_function__geneLengthPaper.py
+++ EM_plot/plot_time_function__geneLengthPaper.py
@@ -7,13 +7,9 @@ import sys
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-def Lg(t):
+def f(t):
    k = 0.0013561601380
    return 452 * np.exp(k * (3700-t))
-
-def mya(Lg):
-   k = 0.0013561601380
-   return (np.log(452) - np.log(Lg) + k * 3700)/k
 
 
 start = 0
@@ -25,52 +21,16 @@ t = np.linspace(start, end, count)
 t = np.append(t, [3700.])
 end   = 3700
 print ("t:", t)
-print ("length:", Lg(t))
-df = pd.DataFrame({"t": t, "length": Lg(t)})
+print ("length:", f(t))
+df = pd.DataFrame({"t": t, "length": f(t)})
 print(df)
 
-plt.plot(t, Lg(t), '--', markersize=1, color='pink', label='log')
-plt.plot(t, Lg(t), '.', markersize=5, color='red', label='log')
+plt.plot(t, f(t), '--', markersize=1, color='pink', label='log')
+plt.plot(t, f(t), '.', markersize=5, color='red', label='log')
 plt.title("Mean gene length dependence with time")
 plt.xlabel("t [Mya]")
 plt.ylabel("$<L_{g}(t)>$ [nt]")
-#plt.show()
+plt.show()
 
 # Puedo mostrar automaticamente la especie mas cercana a esa distancia. Mirar:
 # https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
-
-
-
-LgMin            = np.array([6328.07101316192, 20276.2457972491, 24989.2293819869,
-                             22116.5949857129, 31702.7753590552,
-                             36854.496308828]).round(decimals=1)
-LgMax            = np.array([71400.6717131956, 31443.2530905547, 57440.5056160653,
-                             62135.9179115892, 31702.7753590552,
-                             68287.0898077693]).round(decimals=1) 
-LgMiddle           = (LgMax + LgMin) / 2.
-LgMiddle = LgMiddle.round(decimals=1)
-clades = np.array(["fishes", "birds", "reptiles", "mammals",
-                   "anfibios", "primates"])
-
-origin_mya_googleQuickSearch = np.array([530., 165., 320., 225., 400., 55.]).round(decimals=1) 
-
-time_LgMin = mya(LgMin).round(decimals=1)
-time_LgMax = mya(LgMax).round(decimals=1)
-time_LgMiddle = mya(LgMiddle).round(decimals=-1)
-time_LgMin = mya(LgMin).astype(int)
-time_LgMax = mya(LgMax).astype(int)
-time_LgMiddle = mya(LgMiddle).astype(int)
-df2 = pd.DataFrame({"clades": clades,
-                  "LgMin":  LgMin,
-                  "LgMax":  LgMax,
-                  "LgMiddle": LgMiddle,
-                  "time_LgMin": time_LgMin,
-                  "time_LgMax": time_LgMax,
-                  "time_LgMiddle": time_LgMiddle,
-                  "origin_mya_googleQuickSearch": origin_mya_googleQuickSearch}
-                  )
-                                  
-print(df2[["clades", "LgMin", "LgMax", "LgMiddle"]])
-print(df2[["clades", "time_LgMiddle", "time_LgMin", "time_LgMax", "origin_mya_googleQuickSearch"]])
-
-
