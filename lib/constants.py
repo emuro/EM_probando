4824ecde69175710f9e199d931516b90bc33322d
emuro <emmuro@gmail.com>
2023-12-13 17:02:38 +0100

    main work

---
+++ lib/constants.py
@@ -0,0 +1,44 @@
+# python3
+# ################################################################## #
+# constants.py (C) 2023 Mainz.
+# Author: Enrique M. Muro
+# ################################################################## #
+#
+# ------------------------------------------------------------------------
+# Project: gene_length
+#
+# Purpose: Constants used in the project
+# ################################################################## #
+
+import os
+import matplotlib
+
+
+system = list(os.uname())[0]
+if system == 'Linux':
+    base_path_in = "/media/emuro/Nubya/"
+elif system == 'Darwin':
+    base_path_in = "/Volumes/Nubya/" # or Wes
+#
+
+
+#
+MAIN_TABLES_PATH        = "../main_tables/"
+GENES_PROTS_LENGTH_PATH =  base_path_in + "results/geneLength/"
+
+STAT_G_FILE = "stat_protCodGenes.tsv" 
+STAT_P_FILE = "stat_proteins.tsv"
+
+
+#
+COLOR_FOR_DIST = {
+    "genes":    matplotlib.colors.to_hex("#76bdfb", keep_alpha=False),
+    "proteins": matplotlib.colors.to_hex("#ffab98", keep_alpha=False)
+}
+
+ORG_GROUPS       = ["bacteria", "archaea","protists", "plants", "fungi", "metazoa", "vertebrates"]
+COLOR_ORG_GROUPS        = ['#F4B183', '#FFFFFF', '#FFF2CC', '#385723', '#9DC3E6', '#D0A8CD', '#F997CE']
+COLOR_BORDER_ORG_GROUPS = ['#D26E2A', '#000000', '#BF9000', '#A9D18E', '#3B64AD', '#9664A0', '#C00000']
+#
+OLD_COLOR_ORG_GROUPS    = ['#D83B01', '#002050', '#A80000', '#FFA500', '#107C10',
+                           '#EF008C', '#0078D7', '#B4009E']
