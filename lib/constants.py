9de77d250a87e8ead40dfde43186d930a3e0997c
emuro <emmuro@gmail.com>
2023-12-14 19:46:53 +0100

    main_work

--- lib/constants.py
+++ lib/constants.py
@@ -12,6 +12,8 @@
 
 import os
 import matplotlib
+import sys
+
 
 
 system = list(os.uname())[0]
@@ -19,16 +21,15 @@ if system == 'Linux':
     base_path_in = "/media/emuro/Nubya/"
 elif system == 'Darwin':
     base_path_in = "/Volumes/Nubya/" # or Wes
-#
 
-
+GIT_PROJECT_PATH        = os.path.dirname(__file__) + "/../" 
+MAIN_TABLES_PATH        = GIT_PROJECT_PATH + "main_tables/"
 #
-MAIN_TABLES_PATH        = "../main_tables/"
 GENES_PROTS_LENGTH_PATH =  base_path_in + "results/geneLength/"
-
-STAT_G_FILE = "stat_protCodGenes.tsv" 
-STAT_P_FILE = "stat_proteins.tsv"
-
+#
+STAT_G_FILE      = "stat_protCodGenes.tsv" 
+STAT_P_FILE      = "stat_proteins.tsv"
+STAT_MERGED_FILE = "stat_merged.tsv"
 
 #
 COLOR_FOR_DIST = {
@@ -36,7 +37,8 @@ COLOR_FOR_DIST = {
     "proteins": matplotlib.colors.to_hex("#ffab98", keep_alpha=False)
 }
 
-ORG_GROUPS       = ["bacteria", "archaea","protists", "plants", "fungi", "metazoa", "vertebrates"]
+ORG_GROUPS       = ["bacteria", "archaea","protists", "plants", "fungi", "invertebrates", "vertebrates"]
+ORG_KINGDOMS     = ['bacteria', 'archaea', 'eukaryota']
 COLOR_ORG_GROUPS        = ['#F4B183', '#FFFFFF', '#FFF2CC', '#385723', '#9DC3E6', '#D0A8CD', '#F997CE']
 COLOR_BORDER_ORG_GROUPS = ['#D26E2A', '#000000', '#BF9000', '#A9D18E', '#3B64AD', '#9664A0', '#C00000']
 #
