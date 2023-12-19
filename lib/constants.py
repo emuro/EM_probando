1dc0a064335e618f3b219f93fcb8f55081e517a8
emuro <emmuro@gmail.com>
2023-12-19 19:29:33 +0100

    main_work

--- lib/constants.py
+++ lib/constants.py
@@ -41,15 +41,15 @@ STAT_MERGED_FILE   = MAIN_TABLES_PATH + "stat_merged.tsv"
 G_NCBI_GENOME_DATA_FILE       = EXTRA_TABLES_PATH + "stat_protCodGenes_with_ncbiGenomeData.tsv"
 WRONG_ANNOTATIONS_MERGED_FILE = EXTRA_TABLES_PATH + "noisy_stat_merged.tsv"
 
-#
+
+
 COLOR_FOR_DIST = {
     "genes":    matplotlib.colors.to_hex("#76bdfb", keep_alpha=False),
     "proteins": matplotlib.colors.to_hex("#ffab98", keep_alpha=False)
 }
-
-#ORG_GROUPS       = ["bacteria", "archaea","protists", "plants", "fungi",
-#                    "invertebrates", "vertebrates"]
-
+#
+ORG_KINGDOMS            = ['archaea',  'bacteria', 'eukaryota']
+COLOR_KINGDOMS          = ['#D83B01',  '#002050',   '#0078D7']
 #
 ORG_GROUPS       = ["bacteria", "archaea", "protists", "fungi", "plants",
                     "invertebrates", "vertebrates"]
@@ -57,13 +57,32 @@ COLOR_OF ={
     "bacteria": '#D83B01', "archaea": '#002050',
     "protists": '#FFA500', "fungi": '#A80000',
     "plants": '#107C10',
-    "invertebrates": '#EF008C', "vertebrates": '#0078D7'  #, '#B4009E'
+    "invertebrates": '#EF008C', "vertebrates": '#0078D7'
 }
-OLD_COLOR_ORG_GROUPS = []
+# Article colors
+################
+#ARTICLE_COLOR_ORG_GROUPS        = ['#F4B183', '#FFFFFF', '#FFF2CC', '#385723', '#9DC3E6',
+#                                   '#D0A8CD', '#F997CE']
+#ARTICLE_COLOR_BORDER_ORG_GROUPS = ['#D26E2A', '#000000', '#BF9000', '#A9D18E', '#3B64AD',
+#                                   '#9664A0', '#C00000']
+BOOL_USE_ARTICLE_COLORS = 0 # Apply borders for better results
+if BOOL_USE_ARTICLE_COLORS:
+    COLOR_OF = { # update to draft colors
+        "bacteria": '#F4B183', "archaea": '#FFFFFF',
+        "protists": '#FFF2CC', "fungi": '#9DC3E6',
+        "plants": '#385723',
+        "invertebrates": '#9664A0', "vertebrates": '#C00000'  #, '#B4009E'
+    }
+
+    
+COLOR_ORG_GROUPS = []
 for g in COLOR_OF:
-    OLD_COLOR_ORG_GROUPS.append(COLOR_OF[g])
+    COLOR_ORG_GROUPS.append(COLOR_OF[g])
 
 
+
+
+    
 ################################# alpha start    
 # ALPHA: not in use at the moment
 ################################# 
@@ -82,14 +101,5 @@ ALPHA_ORG_GROUPS = np.array(
 ################################# alpha end
 
 
-# Article colors
-################
-COLOR_ORG_GROUPS        = ['#F4B183', '#FFFFFF', '#FFF2CC', '#385723', '#9DC3E6',
-                           '#D0A8CD', '#F997CE']
-COLOR_BORDER_ORG_GROUPS = ['#D26E2A', '#000000', '#BF9000', '#A9D18E', '#3B64AD',
-                           '#9664A0', '#C00000']
 
-
-ORG_KINGDOMS            = ['archaea',  'bacteria', 'eukaryota']
-COLOR_KINGDOMS          = ['#D83B01',  '#002050',   '#EF008C'] #'#4475B4']
 
