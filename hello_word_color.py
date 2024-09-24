5589200dc70c22a7f5c5600d9b0c78ee6ca9fdc3
emuro <emmuro@gmail.com>
2024-09-24 15:28:59 +0200

    test

--- hello_word_color.py
+++ hello_word_color.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Style
+
 print(f"{Fore.RED}Hallo Ole!")
 print(f"{Fore.YELLOW}Du bist herzlich willkommen")
 print(f"{Fore.RED}Alles zu geben und lang lebe der Ole!{Style.RESET_ALL}")
--- logNorm_test.py
+++ logNorm_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
-import statistics
+#import statistics
 import math
 import sys
 
